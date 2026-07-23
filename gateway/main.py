bed6a22df8ae986f9dd41f9746762ebca14c6846
Godswill Ezeala <104563882+Bobbyjsx@users.noreply.github.com>
2026-07-23 15:13:52 +0100

    fix: lint

--- gateway/main.py
+++ gateway/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from gateway.core.config import settings, http_client
 from gateway.api.api import api_router
-from gateway.core.logger import logger
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 
